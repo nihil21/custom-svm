ae9bce734454b7368ff7df078c26c4f459733869
nihil <mattia.orlandi@studio.unibo.it>
2020-03-23 17:53:07 +0100

    Added accuracy scores

--- custom-svm/svm_classify.py
+++ custom-svm/svm_classify.py
@@ -1,10 +1,22 @@
 import argparse
 from svm import SVM
 from data.sample_data_generator import *
+from sklearn.metrics import accuracy_score
 
 RND = 42
 N_SAMP = 200
 N_FEAT = 2
+
+
+def fit_and_predict(svm: SVM,
+                    X_train: np.ndarray,
+                    X_test: np.ndarray,
+                    y_train: np.ndarray,
+                    y_test: np.ndarray):
+    svm.fit(X_train, y_train)
+    y_pred = svm.predict(X_test)
+    print('Accuracy: {0:.2f}%'.format(accuracy_score(y_test, y_pred) * 100))
+    svm.plot2D(np.concatenate((X_train, X_test)), np.concatenate((y_train, y_test)))
 
 
 def main():
@@ -32,23 +44,24 @@ def main():
         except ValueError:
             print("Cannot convert {0:s} to floating point; default 'r' will be used".format(args['r']))
 
-    print('---------- Linear case ----------')
+    print('-' * 60)
+    print('\tLinear case')
+    print('=' * 60)
     X_train, X_test, y_train, y_test = linear_data_generator(n_samples=N_SAMP,
                                                              n_features=N_FEAT,
                                                              random_state=RND)
     svm = SVM()
-    svm.fit(X_train, y_train)
-    svm.plot2D(X_train, y_train)
-    svm.plot2D(X_test, y_test)
+    fit_and_predict(svm, X_train, X_test, y_train, y_test)
+    print('-' * 60)
 
-    print('---------- Non-linear case: circles ----------')
+    print('\tNon-linear case')
+    print('=' * 60)
     svm_params['kernel'] = 'rbf'
     X_train, X_test, y_train, y_test = non_linear_data_generator(n_samples=N_SAMP,
                                                                  random_state=RND)
     svm = SVM(**svm_params)
-    svm.fit(X_train, y_train)
-    svm.plot2D(X_train, y_train)
-    svm.plot2D(X_test, y_test)
+    fit_and_predict(svm, X_train, X_test, y_train, y_test)
+    print('-' * 60)
 
 
 if __name__ == '__main__':
