d99c9cbd98e15b4a9a98106cada7d704b3c691b3
Lostefra <lorenzomario.amorosa@studio.unibo.it>
2020-03-23 12:00:28 +0100

    added fit function for multiclass svm

--- custom-svm/multiclass_svm.py
+++ custom-svm/multiclass_svm.py
@@ -16,25 +16,58 @@ class MulticlassSVM:
                  gamma: Optional[float] = None,
                  deg: Optional[int] = 3,
                  r: Optional[float] = 0.0):
-        # Note: the number of binary SVM classifiers needed will be known only when the dataset labels will be given
-
         # self.SVMs: list of tuples, each one of 3 elements: (SVM_binary_classifier, 1st_class_label, 2nd_class_label)
+        #            1st_class_label corresponds to sign "-", 2nd_class_label to sign "+"
+        # Note: the number of binary SVM classifiers needed will be known only when the dataset labels will be given
         self.SVMs = []
-        # By default RBF kernel is used
+        # By default linear kernel is used
         self.kernel = kernel
         # If gamma is None, it will be computed during fit process
         self.gamma = gamma
         self.deg = deg
         self.r = r
+        self.labels = None
 
     def fit(self, X: np.ndarray, y: np.ndarray):
-        labels = np.unique(y)
-        for i in range(0, labels.shape[0] - 1):
-            for j in range(i + 1, labels.shape[0]):
-                current_dataset_index = y == labels[i] or y == labels[j]
+        # check if labels are integers
+        labels = np.unique()
+        for label in labels:
+            if not label.is_integer():
+                raise ValueError(label + " is not an integer value label")
+        self.labels = np.array(labels, dtype=int)
+
+        for i in range(0, self.labels.shape[0] - 1):
+            for j in range(i + 1, self.labels.shape[0]):
+                current_dataset_index = y == self.labels[i] or y == self.labels[j]
                 current_X = X[current_dataset_index]
-                current_y = y[current_dataset_index]
+                current_y = np.fromiter((-1 if yi == self.labels[i] else 1 for yi in y[current_dataset_index]), y.dtype)
                 svm = SVM(kernel=self.kernel, gamma=self.gamma, deg=self.deg, r=self.r)
                 svm.fit(current_X, current_y)
-                svm_tuple = (svm, labels[i], labels[j])
+                svm_tuple = (svm, self.labels[i], self.labels[j])
                 self.SVMs.append(svm_tuple)
+
+    def predict(self, X: np.ndarray):
+        """The voting process is based on the standard predict function for binary SVM classifiers, so the input entry
+           is assigned to the class which wins the highest number of binary comparisons.
+           Anyway, to counteract the possible risk of draw, the predicted value before the application of 'sign'
+           function in binary classifiers is stored as well. These latter values are used to deal with draws.
+           For each label i:
+           - voting_schema[0][i] is the number of total comparisons won
+           - voting_schema[1][i] is the cumulative sum of predicted values"""
+        voting_schema = np.zeros([self.labels.shape[0], 2], dtype=float)
+        for svm_tuple in self.SVMs:
+            prediction = svm_tuple[0].predict_value(X)
+            if prediction < 0:
+                voting_schema[0][svm_tuple[1]] += 1
+                voting_schema[1][svm_tuple[1]] += -1 * prediction
+            else:
+                voting_schema[0][svm_tuple[2]] += 1
+                voting_schema[1][svm_tuple[2]] += prediction
+
+        sorted_votes = np.sort(voting_schema[0])
+        # if the first two classes received a different number of votes there is no draw
+        if sorted_votes[0] > sorted_votes[1]:
+            return voting_schema[0].argmax()
+        # otherwise return label of the class which has highest cumulative sum of predicted values
+        else:
+            return voting_schema[1].argmax()
