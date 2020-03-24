1035e48171762585c2c2da9a105d34c9d945b140
nihil <mattia.orlandi@studio.unibo.it>
2020-03-24 16:03:38 +0100

    Changed the way in which linearly separable dataset is generated

--- custom-svm/data/sample_data_generator.py
+++ custom-svm/data/sample_data_generator.py
@@ -1,20 +1,16 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.datasets import make_circles
+from sklearn.datasets import make_blobs, make_circles
 from typing import Optional
 
 
 def linear_data_generator(n_samples: int,
                           n_features: int,
                           random_state: Optional[int] = None) -> (np.ndarray, np.ndarray, np.ndarray, np.ndarray):
-    rnd_state = np.random.RandomState(seed=random_state)
-    X = rnd_state.rand(n_samples, n_features)
-    y = np.ones(n_samples, dtype=np.int)
-    # Produce the linear dataset s.t. positives and negatives are separated by
-    #   y = x - 0.1
-    for i in range(n_samples):
-        if (X[i, 0] - 0.1) > X[i, 1]:
-            y[i] = -y[i]
+    X, y = make_blobs(n_samples, n_features, centers=2, cluster_std=2.0, random_state=random_state)
+    # Assign '-1' instead of '0'
+    is_zero = y < 1
+    y[is_zero] = -1
     return train_test_split(X, y, random_state=random_state)
 
 
